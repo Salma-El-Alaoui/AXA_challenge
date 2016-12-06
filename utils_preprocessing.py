166c35777752b1d8159d9159de7a8815e5492e0d
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2016-12-06 18:48:45 +0100

    add useful function

--- utils_preprocessing.py
+++ utils_preprocessing.py
@@ -5,6 +5,7 @@ Created on Tue Nov 29 10:47:45 2016
 @author: juliette
 """
 import numpy as np
+import pandas as pd
 from datetime import datetime as dt
 from challenge_constants import *
 
@@ -40,4 +41,22 @@ def fill_holidays(table, holiday_column='DAY_OFF', date_column='DATE', holiday= 
     table[holiday_column]=table[date_column].apply(lambda x : str(x.date()) in holiday).astype(int)
     return table
     
+def get_test_data():  
+    lines = open("data/submission.txt", "r", encoding="utf-8").readlines()
+    dates = []
+    assignments = []
+    d = dict.fromkeys(['DATE', 'DATE_FORMAT', 'ASS_ASSIGNMENT'])
+    for line in lines[1:]:
+        row = line.split("\t")
+        dates.append(row[0])
+        assignments.append(row[1])
     
+    d['DATE'] = dates
+    d['DATE_FORMAT'] = dates
+    d['ASS_ASSIGNMENT'] = assignments
+    
+    df_test = pd.DataFrame(data=d) 
+    df_test = date_to_str(df_test, ['DATE_FORMAT'])
+    df_test = date_format(df_test, ['DATE_FORMAT'])
+    
+    return df_test
