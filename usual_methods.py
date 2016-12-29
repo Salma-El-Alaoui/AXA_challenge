c881a72df1b71905631149fa3175c366ab46e955
camille <camille.jandot@gmail.com>
2016-12-29 01:23:19 +0100

    Code for submission with RF. Made month and week of day dummy, and transform hour and minute to continuous time

--- usual_methods.py
+++ usual_methods.py
@@ -100,24 +100,54 @@ def get_train_test(first_day_week,data,sub_data):
     test_set = sub_data.loc[sub_data.DATE >= first_day_week].loc[sub_data.DATE <= last_day_week]
     test_set.loc[test_set.DATE.isnull(), 'DATE'] = test_set[test_set.DATE.isnull()].index
     train_set = data.loc[data['DATE'] < first_day_week]
+    train_set.loc[train_set.DATE.isnull(), 'DATE'] = train_set[train_set.DATE.isnull()].index
     return (train_set, test_set)
     
 #%%  For testing purpose   
 first_day_week = sub_dates['Japon'][2]
 t = get_train_test(first_day_week,no_duplicates['Japon'],sub_data[sub_data.ASS_ASSIGNMENT == 'Japon'])
-
+print(datetime.datetime.combine(first_day_week, datetime.time(00, 00, 00)).minute)
 #%% Feature extraction
 
+def fill_inexistant_features(train_features,test_features):
+    """
+    For each week we need to predict, there is only only one month (or two), so 
+    only one (or two) dummy features are created when dealing with the month 
+    categorical data in extract_features. To train a model, we need to have the
+    same features in train set and in test set. 
+    
+    This function adds to test_features the features that are present in train_
+    features but not in test_features, filling them with 0s.
+    """
+    n = len(test_features['YEAR'])
+    columns_train_features = train_features.columns
+    columns_test_features = test_features.columns
+    for column in columns_train_features:
+        if column not in columns_test_features:
+            test_features[str(column)] = np.zeros(n)
+    return test_features
+    
 def extract_features(input_data):
     data = input_data.copy()
     data['YEAR'] = data.DATE.apply(lambda x: x.year) 
     data['MONTH'] = data.DATE.apply(lambda x: x.month) 
+    data_dummy_month = pd.get_dummies(data['MONTH'],prefix='MONTH')
     data['DAY'] = data.DATE.apply(lambda x: x.day) 
     data['WEEK_DAY'] = data.DATE.apply(lambda x: x.weekday()) 
+    data_dummy_weekday = pd.get_dummies(data['WEEK_DAY'],prefix='WEEKDAY')
     data['HOUR'] = data.DATE.apply(lambda x: x.hour) 
-    data['MINUTE'] = data.DATE.apply(lambda x: x.minute) 
+    data['MINUTE'] = data.DATE.apply(lambda x: x.minute * 5./300.) # .minute gives 0 or 30,
+    #and to have continuous times, we want 30 to becomes half an hour, ie. 0.5
+    data['TIME'] = data['HOUR'] + data['MINUTE']
     data = fill_holidays(data)
-    return data
+    data.drop('HOUR', axis=1, inplace=True)
+    data.drop('MINUTE', axis=1, inplace=True)
+    data.drop('MONTH', axis=1, inplace=True)
+    data.drop('WEEK_DAY', axis=1, inplace=True)
+    data.drop('DATE', axis=1, inplace=True)
+    data_with_dummies = data.join(data_dummy_month).join(data_dummy_weekday)
+    return data_with_dummies
+
     
 def extract_labels(input_data):
     return input_data.CSPL_RECEIVED_CALLS
@@ -128,6 +158,10 @@ data_test_ex = sub_data[sub_data.ASS_ASSIGNMENT == 'Japon'].copy()
 train_features = extract_features(data_train_ex)
 train_labels = extract_labels(data_train_ex)
 test_features = extract_features(data_test_ex)
+print(train_features.columns)
+#print(len(np.unique(train_features['WEEKDAY_0'])))
+print((np.unique(train_features['WEEKDAY_0'])))
+print((np.unique(train_features['TIME'])))
 
 #%% Loss function and scoring method
 
@@ -137,12 +171,42 @@ def custom_loss(estimator,x, y):
     return np.mean(diff)
     
 
-#%% Build models, one model per assignment and per week.
-# TODO: see if it is different with a unique model for all assignments. Assignment
-# would then become a feature
+#%% dataframe for submission
 
-output_df = pd.read_csv("data/submission.txt", encoding="utf-8", sep = "\t")
-print(output_df)
+
+lines = open("data/submission.txt", "r", encoding="utf-8").readlines()
+dates = []
+assignments = []
+predictions = []
+
+d = dict.fromkeys(['DATE', 'DATE_FORMAT', 'ASS_ASSIGNMENT','prediction'])
+
+for line in lines[1:]:
+    row = line.split("\t")
+    dates.append(row[0])
+    assignments.append(row[1])
+    predictions.append(row[2])
+
+d['DATE'] = dates
+d['DATE_FORMAT'] = dates
+d['ASS_ASSIGNMENT'] = assignments
+d['prediction'] = 0
+ 
+df_test = pd.DataFrame(data=d)
+
+df_test = date_to_str(df_test, ['DATE_FORMAT'])
+df_test = date_format(df_test, ['DATE_FORMAT'])
+print(df_test)
+
+#%% Test writing to output_df
+
+
+one_date = datetime.datetime.combine(sub_dates[sub_assignments[0]][0], datetime.time(00, 00, 00))
+index = [sub_assignments[0],one_date]
+print(df_test.loc[(df_test["DATE_FORMAT"] == one_date)])
+df_test.loc[(df_test["DATE_FORMAT"] == one_date) & (df_test["ASS_ASSIGNMENT"] == sub_assignments[0]) , "prediction"] = 1
+#output_df.set_value(index=index, col="prediction", value=1)
+print(df_test.head(2))
 #%%
 
 cv_score = []
@@ -152,27 +216,46 @@ for assignment in sub_assignments:
     print("Model for assignment " + str(assignment))
     n_predictions = len(sub_dates[assignment])
     for i,first_day in enumerate(sub_dates[assignment]):
-        print("** Week no " +  str(i) + " out of " + str(n_predictions))
+        print("** Week no " +  str(i + 1) + " out of " + str(n_predictions))
         ## Building train and test sets
         train_set, test_set = get_train_test(first_day,no_duplicates[assignment],sub_data[sub_data.ASS_ASSIGNMENT == assignment]) 
         ## Extracting features and labels
         train = extract_features(train_set)
+        test_set.drop('ASS_ASSIGNMENT', axis=1, inplace=True)
+        test_set.drop('DATE_FORMAT', axis=1, inplace=True)
         test = extract_features(test_set)
-        train_features = train[['YEAR','MONTH','DAY','WEEK_DAY','HOUR','MINUTE','DAY_OFF']]
-        test_features = test[['YEAR','MONTH','DAY','WEEK_DAY','HOUR','MINUTE','DAY_OFF']]
+        train_features = train     # to delete    
+        test_features = test       # to delete
+        train_features.drop('CSPL_RECEIVED_CALLS', axis=1, inplace=True)
+        test_features = fill_inexistant_features(train_features,test_features)
+        train_features.sort(axis=1, ascending=True, inplace=True)
+        test_features.sort(axis=1, ascending=True, inplace=True)
         train_labels = extract_labels(train_set)
         ## Model
         clf = RandomForestRegressor(n_estimators = 100)
-        #cv_score_assignment.append(cross_val_score(clf,train_features.as_matrix(),train_labels.as_matrix(),cv=5,scoring=custom_loss).mean())
-        clf.fit(train_features.as_matrix(),train_labels.as_matrix())
-        test["PRED"] = clf.predict(test_features.as_matrix())
-        print(test[["DATE","ASS_ASSIGNMENT","PRED"]])
-        ## Write predictions to file
-        
-        # TODO: write predictions to file
+#        #cv_score_assignment.append(cross_val_score(clf,train_features.as_matrix(),train_labels.as_matrix(),cv=5,scoring=custom_loss).mean())
+        train_features_matrix = train_features.as_matrix()
+        train_labels_matrix = train_labels.as_matrix()
+        clf.fit(train_features_matrix,train_labels_matrix)
+        ypred = clf.predict(test_features.as_matrix())
+        ## Write predictions to dataframe        
+        for i,date in enumerate(test_set["DATE"]):
+            df_test.loc[(df_test["DATE_FORMAT"] == date) & (df_test["ASS_ASSIGNMENT"] == assignment) , "prediction"] = ypred[i]
+
         
         
     #cv_score.append(cv_score_assignment)
     #print(cv_score_assignment)
         
-        
+print(df_test['predictions'])       
+
+
+# %% Write to sumbission file
+
+d_sub = df_test[['DATE', 'ASS_ASSIGNMENT', 'prediction']]
+d_sub.to_csv('data/test_submission_rf_3.csv', sep="\t", encoding='utf-8', index=False)
+
+# %%
+d_sub_2=d_sub.copy()
+d_sub_2['prediction'] = 2 * d_sub_2['prediction']
+d_sub_2.to_csv('data/test_submission_rf_2.csv', sep="\t", encoding='utf-8', index=False)
